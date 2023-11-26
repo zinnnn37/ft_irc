89eb78d6f085b4d99465325c2c5c200993bbb7bc
zinnnn37 <zinnnn37@gmail.com>
2023-11-26 11:12:11 +0900

    [IRC] split && check number of params

--- Cmd_USER.cpp
+++ Cmd_USER.cpp
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 10:10:07 by minjinki          #+#    #+#             */
-/*   Updated: 2023/11/26 11:00:28 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/11/26 11:11:11 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,24 @@ void	Command::_split( Client *client, std::istringstream &iss, std::string *spli
 {
 	int			i = 0;
 	std::string	line;
-	std::string	user = "USER";
 
 	while (i < 4)
 	{
-		iss >> line;
-
-		if (line.empty())
+		if (!(iss >> line))
 		{
-			client->setSendData(ERR_NEEDMOREPARAMS(user));
+			client->setSendData(ERR_NEEDMOREPARAMS(std::string("USER")) + CRLF);
 			return ;
 		}
 
 		if (line[0] == ':')
 		{
 			line = line.substr(1);
-			split[i++] = line;
 
 			if (line.length() == 0)
-			{
-				iss >> line;
-				split[3] = line;
-			}
+				continue ;
 		}
 		split[i++] = line;
+		std::cout << i-1 << line << std::endl;
 	}
 }
 void	Command::user( Client *client, std::istringstream &iss )
@@ -48,6 +42,14 @@ void	Command::user( Client *client, std::istringstream &iss )
 	std::string	split[4];
 
 	this->_split(client, iss, split);
+	std::cout << std::endl;
 
-	
+	for (int i = 0; i < 4; i++)
+		std::cout << i << " " << split[i] << std::endl;
+
+	if (split[0].empty() || split[1].empty() || split[2].empty() || split[3].empty())
+	{
+		client->setSendData(ERR_NEEDMOREPARAMS(std::string("USER")) + CRLF);
+		return ;
+	}
 }
